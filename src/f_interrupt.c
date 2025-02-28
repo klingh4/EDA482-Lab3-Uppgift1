9478787bcfb82af54c0e4de6caff13c41e0b292e
Samuel H. Kling <klingen04@gmail.com>
2025-02-28 11:37:08 +0100

    InitSquareWave() påbörjad, kanske klar

--- src/f_interrupt.c
+++ src/f_interrupt.c
@@ -1,11 +1,15 @@
 #include "h_interrupt.h"
+#include "h_header.h"
 
 void InitSquareWave() {
-
+    *GPIO_E_MODER |= 0x1; //moder 0x1 = output
+    *GPIO_E_OTYPER &= ~0x1; //otyper 0x0 = push-pull
+    *GPIO_E_PUPDR &= ~0x3; //pupdr 0x0 = no pull-up/pull-down
+    GPIO_E_ODR &= ~(0x1); //odr 0x0 = low
 }
 
 void StartSquarewave(unsigned int period_in_us) {
-
+    
 }
 
 void StopSquareWave() {
